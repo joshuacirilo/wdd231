fb525c8d5061a81e5a76c17dfe94b568e1f8b43a
joshuacirilo <u175692795@byui.edu>
2025-09-18 21:59:20 -0600

    fix_bug_with_json_and_add_ccs_to_cards

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,39 +1,71 @@
-const url = '../data/members.json';
+const url = './data/members.json';
+
+
 const cards = document.querySelector('#cards');
 
 async function getMembersData() {
-  try {
     const response = await fetch(url);
-    if (!response.ok) throw new Error('No se pudo cargar el JSON');
 
     const data = await response.json();
 
-    if (data && Array.isArray(data.companies)) {
       displayMembers(data.companies);
-    } else {
-      console.error('El JSON no tiene la propiedad "companies"', data);
-    }
-  } catch (err) {
-    console.error('Error al obtener datos:', err);
-  }
+
+
+
+
+
 }
 
-function displayMembers(companies) {
+      getMembersData()
+
+const displayMembers = (companies) => {
   companies.forEach(company => {
     const card = document.createElement('section');
-    const names = document.createElement('h2');
+    
+
+    // names
+    const name = document.createElement('h2');
+    name.textContent = company.name;
+
+    // img
     const portrait = document.createElement('img');
-
-    names.textContent = company.name;
     portrait.src = company.image;
-    portrait.alt = `Portrait of ${company.name}`;
+    portrait.alt = `Logo of ${company.name}`;
     portrait.loading = 'lazy';
-    portrait.width = 340;
-    portrait.height = 440;
+    portrait.width = 120;
+    portrait.height = 120;
 
-    card.append(names, portrait);
+    // address
+    const address = document.createElement('p');
+    address.textContent = `📍 ${company.address}`;
+
+    // phone
+    const phone = document.createElement('p');
+    phone.textContent = `📞 ${company.phone}`;
+
+    // Website
+    const website = document.createElement('a');
+    website.href = company.website;
+    website.target = "_blank";
+    website.textContent = "🌐 Visit Website";
+
+    // Industry
+    const industry = document.createElement('p');
+    industry.textContent = `🏭 Industry: ${company.industry}`;
+
+    // Contact Person
+    const contact = document.createElement('p');
+    contact.textContent = `👤 Contact: ${company.contact_person}`;
+
+    // Email
+    const email = document.createElement('a');
+    email.href = `mailto:${company.email}`;
+    email.textContent = `✉️ ${company.email}`;
+
+    
+    card.append(name, portrait, address, phone, website, industry, contact, email);
+
+    
     cards.appendChild(card);
   });
-}
-
-getMembersData();
+};
